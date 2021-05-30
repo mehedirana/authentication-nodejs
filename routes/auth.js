6b3d052eba03e0fe6e2aebbc57f788234c2d677d
mehedirana <mdmehedirana@gmail.com>
2021-05-30 21:11:14 +0600

    validation js created in app directory

--- routes/auth.js
+++ routes/auth.js
@@ -3,32 +3,28 @@ const User = require('../model/User');
 const bodyParser = require('body-parser');
 const Joi = require('joi');
 
-const schema = Joi.object({
-    name: Joi.string().min(6).max(30).required(),
-    email: Joi.string().min(6).max(30).email().required(),
-    password: Joi.string().min(6).required(),
-})
 
 router.post('/register', async (req, res)=>{
     //lets validate user
     const {error} = schema.validate(req.body)
-    res.send(error)
-    console.log(error);
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
 
-    // });
-    // // console.log('user', user);
-    // try {
-    //     const savedUser = await user.save();
-    //     console.log(`saved User: ${savedUser}`);
-    //     res.send(savedUser);
+    if(error) res.status(400).send(error.details[0].message)
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+
+    });
+    
+    try {
+        const savedUser = await user.save();
+        // console.log(`saved User: ${savedUser}`);
+        res.send(savedUser);
         
-    // } catch (error) {
-    //     res.status(400).send(error)
-    // }
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
 module.exports = router;
