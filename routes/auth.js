363c5d0a2f9318dbe8aebd41bd99350784ea3073
mehedirana <mdmehedirana@gmail.com>
2021-05-30 20:41:22 +0600

    validation working

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,17 @@ const User = require('../model/User');
 const bodyParser = require('body-parser');
 const Joi = require('joi');
 
-const schema = {
+const schema = Joi.object({
     name: Joi.string().min(6).max(30).required(),
-    email: Joi.string().min(6).max(30).required(),
+    email: Joi.string().min(6).max(30).email().required(),
     password: Joi.string().min(6).required(),
-}
+})
 
 router.post('/register', async (req, res)=>{
     //lets validate user
-    const validation = Joi.validate(req.body, schema)
-    res.send(validation)
-    console.log(validation);
+    const {error} = schema.validate(req.body)
+    res.send(error)
+    console.log(error);
     // const user = new User({
     //     name: req.body.name,
     //     email: req.body.email,
