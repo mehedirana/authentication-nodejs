51a2c137aa4dbf83a7106f1dab4570ed5188f2ec
mehedirana <mdmehedirana@gmail.com>
2021-05-30 21:27:26 +0600

    only new user can sign in

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,9 @@ router.post('/register', async (req, res)=>{
     const {error} = registerValidation(req.body)
 
     if(error) return res.status(400).send(error.details[0].message)
-
+    // cheacking already exists user
+    const emailAlreadyExits = await User.findOne({email: req.body.email})
+    if(emailAlreadyExits)  return res.status(400).send("Email Already exists")
     const user = new User({
         name: req.body.name,
         email: req.body.email,
