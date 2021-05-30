c4328c93d00cd243d73cceb9d1198634094380ed
mehedirana <mdmehedirana@gmail.com>
2021-05-30 21:42:09 +0600

    password hashing done

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const {registerValidation, loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs')
 
 
 router.post('/register', async (req, res)=>{
@@ -11,10 +12,17 @@ router.post('/register', async (req, res)=>{
     // cheacking already exists user
     const emailAlreadyExits = await User.findOne({email: req.body.email})
     if(emailAlreadyExits)  return res.status(400).send("Email Already exists")
+
+    // Password Hashing
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    console.log(hashPassword);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
 
     });
     
